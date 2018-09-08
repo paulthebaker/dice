aab661a8e28e5c5fe12be134015a7a12a53f7620
Paul T. Baker <paultbaker@gmail.com>
2018-09-08 12:21:01 -0400

    rewrite to use Dice class

--- roll.py
+++ roll.py
@@ -14,17 +14,7 @@ import numpy as np
 import argparse
 import re
 
-def is_dice(string):
-    """validates dice string of form '^\d+d\d+\Z'
-      ^ -- start of string
-      \d -- digit
-      + -- at least one (greedy)
-      d -- char 'd'
-      \Z -- end of string
-    """
-    regex = re.compile(r'^\d+d\d+\Z')
-    return bool(regex.match(string))
-
+from dice import Dice
 
 parser = argparse.ArgumentParser(description = 'simulated dice rolling!')
 
@@ -56,58 +46,30 @@ M = args.M  # repeats
 L = args.L  # largest
 S = args.S  # smallest
 
-err = "dice.py: Error: "
-warn = "dice.py: Warning: "
+err = "roll.py: Error: "
+warn = "roll.py: Warning: "
 
-# sanity checks
-if not is_dice(dice):
-    print(err + "'{0:s}' is not a valid dice string (e.g. '4d6')"
-           .format(dice))
-    exit()
+myDice = Dice(args.dice[0], keep_large=L, keep_small=S)
+
 if M<1:
     print(err + "cannot roll fewer than 1 trial")
     exit()
 if M==1 and args.ave:
     print(warn + "computing average of 1 roll")
-if L and S:
-    print(err + "cannot keep both largest and smallest")
-    print("  specify ONE of --large OR --small")
-    exit()
 
-N, D = map(int, dice.split('d'))
+rolls = myDice.roll(num=M)
 
-if L and L>=N:
-    print(warn + "asked for largest {0:d} of {1:d} dice, keeping ALL"
-           .format(L, N))
-    L = None
-if S and S>=N:
-    print(warn + "asked for smallest {0:d} of {1:d} dice, keeping ALL"
-           .format(S, N))
-    S = None
-
-# dice rolls, M trials of NdD
-rolls = np.random.randint(D, size=(M, N)) + 1
-
-
-sums = np.zeros(M, dtype=int)
 if L:
     roll_name = "{0:s} keep largest {1:d}".format(dice, L)
-    keep = np.argsort(rolls, axis=1)
-    for mm, roll in enumerate(rolls):
-        sums[mm] = np.sum( roll[keep[mm]][-L:] )
 elif S:
     roll_name = "{0:s} keep smallest {1:d}".format(dice, S)
-    keep = np.argsort(rolls, axis=1)
-    for mm, roll in enumerate(rolls):
-        sums[mm] = np.sum( roll[keep[mm]][:S] )
 else:
     roll_name = "{0:s}".format(dice)
-    sums = np.sum(rolls, axis=1)
-
+    
 if args.ave:
-    print('ave of {0:s} x{1:d}'.format(dice, M))
-    print('  {:f}'.format(np.mean(sums)))
+    print('ave of {0:s} ({1:d} trials)'.format(roll_name, M))
+    print('  {:f}'.format(np.mean(rolls)))
 else:
     print('{0:s} = '.format(roll_name))
-    for mm, roll in enumerate(sums):
+    for roll in rolls:
         print('  {:4d}'.format(roll))
